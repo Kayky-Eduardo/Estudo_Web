f93ed608ce6f5add2aa38bdb8007338a1020f892
Kayky-Eduardo <kayky002a@gmail.com>
2025-07-16 19:50:40 -0300

    final

--- atividades_js/atividade02/js/script.js
+++ atividades_js/atividade02/js/script.js
@@ -3,23 +3,31 @@ let segundo_nome = prompt("Digite o seu segundo nome: ");
 let terceiro_nome = prompt("Digite o seu terceiro nome: ");
 let possibilidade = prompt("Você possui um quarto nome?(Sim/Não): ");
 
-possibilidade = possibilidade.toLowerCase();
+possibilidade = possibilidade.trim().toLowerCase();
 if (possibilidade == "sim" || possibilidade == "s") {
     let quarto_nome = prompt("Digite seu quarto nome: ");
     
-    let nome_completo = primeiro_nome.concat(" ", segundo_nome, " ", terceiro_nome, " ", quarto_nome);
+    let nome_completo = primeiro_nome.concat(" ", segundo_nome, " ", terceiro_nome, " ", quarto_nome).toLowerCase();
     
     let tamanho = nome_completo.length;
     console.log(`Nome em maiúsculo: ${nome_completo.toUpperCase()}`)
     console.log(`Nome em minúsculo: ${nome_completo.toLowerCase()}`)
     console.log(`O nome ${nome_completo} tem ${tamanho} caracteres`);
 
-    let lista_nome_completo = nome_completo.split(" ");
-    let lista_nome_inverso = lista_nome_completo.reverse();
-    let nome_inverso = lista_nome_inverso[0].concat( " ", lista_nome_inverso[1], " ",
-    lista_nome_inverso[2], " ", lista_nome_inverso[3])
-    
-    console.log(`${nome_completo} de ao contrário é ${nome_inverso}`)
+    let nome_invertido = nome_completo.split("").reverse().join("")    
+    console.log(`${nome_completo} de ao contrário é ${nome_invertido}`)
+
+    let trocar = prompt("Deseja alterar algum nome?(s/n): ").toLowerCase();
+    if (trocar == "s" || trocar == "sim") {
+        let nome_escolhido = prompt("Qual nome deseja alterar? ").toLowerCase();
+        if (nome_completo.includes(nome_escolhido)) {
+            let trocar_nome = prompt("Para qual deseja alterar? ").toLowerCase();
+            nome_completo = nome_completo.replace(nome_escolhido, trocar_nome);
+            console.log(nome_completo)
+        } else {
+            alert("Este nome não foi encontrado");
+        }
+    }
 } else if (possibilidade == "nao" || possibilidade == "n") {
     let nome_completo = primeiro_nome.concat(" ", segundo_nome, " ", terceiro_nome);
 
@@ -28,13 +36,8 @@ if (possibilidade == "sim" || possibilidade == "s") {
     console.log(`Nome em minúsculo: ${nome_completo.toLowerCase()}`)
     console.log(`O nome ${nome_completo} tem ${tamanho} caracteres`);
 
-    let lista_nome_completo = nome_completo.split(" ");
-    let lista_nome_inverso = lista_nome_completo.reverse();
-
-    let nome_inverso = lista_nome_inverso[0].concat( " ", lista_nome_inverso[1], " ",
-    lista_nome_inverso[2]);
-    console.log(`${nome_completo} de ao contrário é ${nome_inverso}`)
-
+    let nome_invertido = nome_completo.split("").reverse().join("")  ;  
+    console.log(`${nome_completo} de ao contrário é ${nome_invertido}`);
 } else {
     console.log("Digite uma resposta válida!")
 }
