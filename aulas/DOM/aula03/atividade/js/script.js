a8fbfe6b158e900a9a5c0a81fd68046543dfa221
Kayky-Eduardo <kayky002a@gmail.com>
2025-08-20 23:32:27 -0300

    atividade

---
+++ aulas/DOM/aula03/atividade/js/script.js
@@ -0,0 +1,37 @@
+const produtos = [
+    { nome: "Sabonete Natural", preco: 15.00, categoria: "higiene" },
+    { nome: "Shampoo Orgânico", preco: 25.00, categoria: "higiene" },
+    { nome: "Granola Artesanal", preco: 18.50, categoria: "alimentacao" },
+    { nome: "Mel Puro", preco: 22.00, categoria: "alimentacao" }
+];
+
+function mostrarItens(filtro) {
+    const lista = document.getElementById("listaDeProdutos") 
+    let soma = 0;
+    let valorTotal = 0;
+    lista.innerHTML = ""
+    produtos.forEach(produto => {
+        if (filtro === "todos" || produto.categoria === filtro) {
+            const item = document.createElement("div")
+            item.className = "produto";
+            item.innerHTML = `
+            Nome:${produto.nome}<br>
+            Preço: ${produto.preco}
+            `
+            lista.appendChild(item)
+            soma += produto.preco
+        } 
+    })
+    
+    const total = document.getElementById('total')
+    valorTotal = soma
+    total.innerHTML = `Valor total: ${valorTotal}`;
+}
+
+const filtro = document.getElementById('filter');
+
+filtro.addEventListener('change', () => {
+    mostrarItens(filtro.value);
+})
+
+
